1d81e478047fad3f4a48cca72fe02e847aa12ac4
Hassan Thameur <hassan.thameur@gmail.com>
2025-04-22 15:56:11 +0200

    Add caching for product retrieval functions: getProductsById, getProductByHandle, and getProductsList.

--- storefront/src/lib/data/products.ts
+++ storefront/src/lib/data/products.ts
@@ -3,6 +3,7 @@
 import { sdk } from "@lib/config"
 import { sortProducts } from "@lib/util/sort-products"
 import { HttpTypes } from "@medusajs/types"
+import { cache } from "react"
 import { SortOptions } from "@modules/store/components/refinement-list/sort-products"
 import { getAuthHeaders, getCacheOptions } from "./cookies"
 import { getRegion, retrieveRegion } from "./regions"
@@ -135,3 +136,87 @@ export const listProductsWithSort = async ({
   }
 }
 
+
+export const getProductsById = cache(async function ({
+  ids,
+  regionId,
+}: {
+  ids: string[]
+  regionId: string
+}) {
+  return sdk.store.product
+    .list(
+      {
+        id: ids,
+        region_id: regionId,
+        fields: "*variants.calculated_price,+variants.inventory_quantity",
+      },
+      { next: { tags: ["products"] } }
+    )
+    .then(({ products }) => products)
+})
+
+export const getProductByHandle = cache(async function (
+  handle: string,
+  regionId: string
+) {
+  return sdk.store.product
+    .list(
+      {
+        handle,
+        region_id: regionId,
+        fields: "*variants.calculated_price,+variants.inventory_quantity",
+      },
+      { next: { tags: ["products"] } }
+    )
+    .then(({ products }) => products[0])
+})
+
+export const getProductsList = cache(async function ({
+  pageParam = 1,
+  queryParams,
+  countryCode,
+}: {
+  pageParam?: number
+  queryParams?: HttpTypes.FindParams & HttpTypes.StoreProductParams
+  countryCode: string
+}): Promise<{
+  response: { products: HttpTypes.StoreProduct[]; count: number }
+  nextPage: number | null
+  queryParams?: HttpTypes.FindParams & HttpTypes.StoreProductParams
+}> {
+  const limit = queryParams?.limit || 12
+  const validPageParam = Math.max(pageParam, 1);
+  const offset = (validPageParam - 1) * limit
+  const region = await getRegion(countryCode)
+
+  if (!region) {
+    return {
+      response: { products: [], count: 0 },
+      nextPage: null,
+    }
+  }
+  return sdk.store.product
+    .list(
+      {
+        limit,
+        offset,
+        region_id: region.id,
+        fields: "*variants.calculated_price",
+        ...queryParams,
+      },
+      { next: { tags: ["products"] } }
+    )
+    .then(({ products, count }) => {
+      const nextPage = count > offset + limit ? pageParam + 1 : null
+
+      return {
+        response: {
+          products,
+          count,
+        },
+        nextPage: nextPage,
+        queryParams,
+      }
+    })
+})
